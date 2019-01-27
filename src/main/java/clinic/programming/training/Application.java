b1b2ea19accd0a68912277b6585b86072e1173c9
Michal <m.baraniak@streamsoft.pl>
2019-01-27 22:47:15 +0100

    Changing pom and app file

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,7 +1,18 @@
 package clinic.programming.training;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Application {
     
+	public void greeat(){
+		List<String> greatings = new ArrayList<>();
+		greatings.add("Hello");
+		
+		for(String greating : greatings){
+			System.out.println("Greating: " + greating);
+		}
+	}
+	
+	
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -9,6 +20,7 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+		Application app = new Application();
+		app.greeat();
     }
 }

af1b81f8600556f280d4d619c8f78fedb24082e4
IwantBeJavaDev <m.baraniak@streamsoft.pl>
2019-01-27 23:35:53 +0100

    Add lang3 dependecy and some code

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,7 +1,14 @@
 package clinic.programming.training;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
+
+	public int countWords(String words){
+		String[] separateWords = StringUtils.split(words,' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
     
 	public void greeat(){
 		List<String> greatings = new ArrayList<>();
@@ -22,5 +29,7 @@ public class Application {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greeat();
+		int count = app.countWords("I have four words");
+		System.out.println("Count: " + count);
     }
 }
